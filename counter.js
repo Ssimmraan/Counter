33a9466a004eac52b128a5088a801854ecb9ec3b
Simran Das <139633566+Ssimmraan@users.noreply.github.com>
2024-12-11 21:55:18 +0530

    new files

---
+++ counter.js
@@ -0,0 +1,22 @@
+let count = 0;
+
+function f1()
+{
+  let x = document.getElementById("b");
+  a -= 1;
+  document.querySelector("#a").innerHTML = a;
+}
+
+function f2()
+{
+  let y = document.getElementById("c");
+  a = 0;
+  document.querySelector("#a").innerHTML = a;
+}
+
+function f3()
+{
+  let z = document.getElementById("d");
+  a += 1;
+  document.querySelector("#a").innerHTML = a;
+}
